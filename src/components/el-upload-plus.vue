248834e02597e0f4926252be3956bb83ff6096c2
Eden <644950436@qq.com>
2024-04-10 23:22:12 +0800

    图片封装

---
+++ src/components/el-upload-plus.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {ref,reactive} from "vue"
+
+const visible = ref(false)
+const fileList = ref([])
+
+const handleRemove = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+}
+
+const handlePictureCardPreview = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url
+  dialogVisible.value = true
+}
+
+const props = defineProps({
+  url:{
+    type:String
+  },
+  multiple:{
+    type:Boolean,
+    default: false
+  },
+  remark:{
+    type:String,
+    default:""
+  }
+})
+</script>
+
+<template>
+  <div>
+    <el-upload
+      v-model="fileList"
+      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove">
+      <el-icon><Plus /></el-icon>
+    </el-upload>
+
+    <el-dialog v-model="visible">
+      <img w-full :src="dialogImageUrl" alt="Preview Image" />
+    </el-dialog>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
--- src/views/modules/setup/content-add-or-update.vue
+++ src/views/modules/setup/content-add-or-update.vue
@@ -1,9 +1,68 @@
 <script setup>
-import {} from "vue"
+import { nextTick, reactive, ref } from "vue"
+import debounce from 'lodash/debounce'
+import { ElMessage } from "element-plus"
+import baseService from "@/service/baseService"
+
+const data = {
+  id: null,
+  name: '',
+  menuIdList: [],
+  remark: ''
+}
+
+const dataForm = reactive({ ...data })
+
+const dataRule = ref({
+  name: [{ required: true, message: "必填项不能为空", trigger: "blur" }]
+})
+
+
 </script>
 
 <template>
-  <div class=""></div>
+  <el-dialog v-model="visible" :title="!dataForm.id ? '新增' : '修改'" :close-on-click-modal="false" :close-on-press-escape="false">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataFormRef" @keyup.enter="dataFormSubmitHandle()" label-width="120px">
+      <el-form-item prop="text1" label="内容1">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="text2" label="内容2">
+        <el-input v-model="dataForm.remark" placeholder="备注"></el-input>
+      </el-form-item>
+      <el-form-item prop="text3" label="内容3">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="text4" label="内容4">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="text5" label="内容5">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="url" label="链接">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="pic" label="图片">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="pic2" label="图片2">
+        <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+      </el-form-item>
+      <el-form-item size="small" label="菜单授权">
+        <el-tree
+          :data="menuList"
+          :props="{ label: 'name', children: 'children' }"
+          node-key="id"
+          ref="menuListTree"
+          accordion
+          show-checkbox>
+        </el-tree>
+      </el-form-item>
+    </el-form>
+    <template v-slot:footer>
+      <el-button @click="visible = false" icon="Close">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmitHandle()" icon="Check">确定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <style lang="scss">
