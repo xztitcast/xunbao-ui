ceaa43455c76e3a2eeac10ffbbe69d67b9158038
Eden <644950436@qq.com>
2024-04-23 23:00:23 +0800

    优化内容分类

--- src/views/modules/setup/content-add-or-update.vue
+++ src/views/modules/setup/content-add-or-update.vue
@@ -6,9 +6,12 @@ import baseService from "@/service/baseService"
 import ElUploadPlus from "@/components/el-upload-plus.vue"
 
 const visible = ref(false)
+const contentCatList = ref([])
+const contentCatTree = ref(null)
 
 const data = {
   id: null,
+  cid: null,
   text1: '',
   text2: '',
   text3: '',
@@ -40,34 +43,41 @@ defineExpose({ init })
   <el-dialog v-model="visible" :title="!dataForm.id ? '新增' : '修改'" :close-on-click-modal="false" :close-on-press-escape="false">
     <el-form :model="dataForm" :rules="dataRule" ref="dataFormRef" @keyup.enter="dataFormSubmitHandle()" label-width="120px">
       <el-form-item prop="text1" label="内容1">
-        <el-input v-model="dataForm.text1" placeholder="名称"></el-input>
+        <el-input v-model="dataForm.text1" placeholder="请填写内容1"></el-input>
       </el-form-item>
       <el-form-item prop="text2" label="内容2">
-        <el-input v-model="dataForm.text2" placeholder="备注"></el-input>
+        <el-input v-model="dataForm.text2" placeholder="请填写内容2"></el-input>
       </el-form-item>
       <el-form-item prop="text3" label="内容3">
-        <el-input v-model="dataForm.text3" placeholder="名称"></el-input>
+        <el-input v-model="dataForm.text3" placeholder="请填写内容3"></el-input>
       </el-form-item>
       <el-form-item prop="text4" label="内容4">
-        <el-input v-model="dataForm.text4" placeholder="名称"></el-input>
+        <el-input v-model="dataForm.text4" placeholder="请填写内容4"></el-input>
       </el-form-item>
       <el-form-item prop="text5" label="内容5">
-        <el-input v-model="dataForm.text5" placeholder="名称"></el-input>
+        <el-input v-model="dataForm.text5" placeholder="请填写内容5"></el-input>
       </el-form-item>
       <el-form-item prop="url" label="链接">
-        <el-input v-model="dataForm.url" placeholder="名称"></el-input>
+        <el-input v-model="dataForm.url" placeholder="请填写URL连接"></el-input>
       </el-form-item>
       <el-form-item prop="pic" label="图片">
-        <el-input v-model="dataForm.pic" placeholder="名称"></el-input>
+        <el-upload-plus v-model="dataForm.pic" :multiple="false"></el-upload-plus>
       </el-form-item>
       <el-form-item prop="pic2" label="图片2">
-        <el-upload-plus v-model="dataForm.pic2" :multiple="false"></el-upload-plus>
+        <el-upload-plus v-model="dataForm.pic2" :alt="请上传大小不超过2MB多张图片"></el-upload-plus>
       </el-form-item>
-      <el-form-item size="small" label="菜单授权">
-        
+      <el-form-item size="small" label="内容分类">
+        <el-tree
+          :data="contentCatList"
+          :props="{ label: 'name', children: 'children' }"
+          node-key="id"
+          ref="contentCatTree"
+          accordion
+          show-checkbox>
+        </el-tree>
       </el-form-item>
     </el-form>
-    <template v-slot:footer>
+    <template #footer>
       <el-button @click="visible = false" icon="Close">取消</el-button>
       <el-button type="primary" @click="dataFormSubmitHandle()" icon="Check">确定</el-button>
     </template>
